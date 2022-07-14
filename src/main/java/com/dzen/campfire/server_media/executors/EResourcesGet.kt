3fb75a7115c42d68746bb109b11a1f28ccbdaad0
sitting33 <me@33rd.dev>
2022-07-14 13:20:47 +0300

    protect images in chat messages, fix EChatMessageChange

--- src/main/java/com/dzen/campfire/server_media/executors/EResourcesGet.kt
+++ src/main/java/com/dzen/campfire/server_media/executors/EResourcesGet.kt
@@ -1,13 +1,13 @@
 package com.dzen.campfire.server_media.executors
 
+import com.dzen.campfire.api.tools.ApiException
 import com.dzen.campfire.api_media.APIMedia
 import com.dzen.campfire.api_media.requests.RResourcesGet
 import com.dzen.campfire.server_media.tables.TResources
-import com.dzen.campfire.api.tools.ApiException
 import com.sup.dev.java_pc.sql.Database
 import com.sup.dev.java_pc.sql.SqlQuerySelect
 
-class EResourcesGet : RResourcesGet(0) {
+class EResourcesGet : RResourcesGet(0, "") {
 
     override fun check() {
 
@@ -15,11 +15,14 @@ class EResourcesGet : RResourcesGet(0) {
 
     override fun execute(): Response {
 
-        val select = SqlQuerySelect(TResources.NAME, TResources.image_bytes)
+        val select = SqlQuerySelect(TResources.NAME, TResources.pwd, TResources.image_bytes)
         select.where(TResources.id, "=", resourceId)
         val v = Database.select("EResourcesGet", select)
 
         if (v.isEmpty) throw  ApiException(APIMedia.ERROR_GONE).salient()
+
+        val correctPwd = v.next<String?>()
+        if (!correctPwd.isNullOrEmpty()) if (correctPwd != pwd) throw ApiException(APIMedia.ERROR_ACCESS)
 
         return Response(v.next())
     }
